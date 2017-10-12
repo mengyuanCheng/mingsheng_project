41c054864f85cad52af7e35e687287d71dd1e9b7
s549520798 <549520798@qq.com>
2017-10-12 11:04:21 +0800

    * zz

--- app/src/main/java/com/grgbanking/ct/activity/MainActivity.java
+++ app/src/main/java/com/grgbanking/ct/activity/MainActivity.java
@@ -225,6 +225,7 @@ public class MainActivity extends Activity {
                 }).execute();
             }
         });
+
         builder.setPositiveButton("取消", new DialogInterface.OnClickListener() {
             @Override
             public void onClick(DialogInterface dialog, int which) {
